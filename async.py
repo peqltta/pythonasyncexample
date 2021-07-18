b850e63706c0669dbc55a0b050a9a98cd6656f03
peqltta <peqlkush@protonmail.com>
2021-07-18 15:31:37 +0100

    asdfasdf

--- async.py
+++ async.py
@@ -1,21 +1,75 @@
 import asyncio
-
-async def fetch_data():
-    print('start fetching')
-    await asyncio.sleep(2)
-    print('done fetching')
-    return {'data': 1}
-async def print_numbers():
-    for i in range(10):
-        print(i)
-        await asyncio.sleep(0.5)
-    
-async def main():
-    task1 = asyncio.create_task(fetch_data())
-    task2 = asyncio.create_task(print_numbers())
-    value = await task1
-    print(value)
-    await task2
-    
-asyncio.run(main())
-    
+import aiohttp
+import re
+import urllib
+clean = re.compile('<.*?>')
+url = 'http://gimmeproxy.com/api/getProxy?protocol=http'
+url2 = 'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
+f = urllib.request.urlopen(url2)
+headers = {'accept': 'application/json',
+'accept-encoding': 'gzip, deflate, br',
+'accept-language': 'en-US,en;q=0.9',
+'cache-control': 'no-cache',
+'pragma': 'no-cache',
+'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+'sec-ch-ua-mobile': '?1',
+'sec-fetch-dest': 'document',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-site': 'none',
+'sec-fetch-user': '?1',
+'upgrade-insecure-requests': '1',
+'user-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0'}
+proxies = []
+for line in f:
+    decoded_line = line.strip().decode("utf-8")
+    proxies.append(decoded_line)
+f.close()
+working = []
+f = open('working.txt','r+')
+for line in f:
+    working.append(line.strip())
+f.close()
+new = []
+async def check(ip):
+    prox = ip
+    ip = "http://" + ip
+    try:
+        session = aiohttp.ClientSession()
+        response = await session.get(url, proxy=ip, headers=headers, timeout=20)
+        out = await response.json()
+        working.append(prox)
+        print(prox + ' ------ working')
+        newproxy = out['ipPort']
+        newproxy = clean.sub('', newproxy)
+        new.append(newproxy)
+        proxies.append(newproxy)
+        print(newproxy + ' ---- new')
+        await session.close()
+    except Exception as e:
+        proxies.remove(prox)
+        print(prox + ' -- dead')
+        await session.close()
+    await session.close()
+tasks = []
+def main():
+    loop = asyncio.get_event_loop()
+    for item in proxies:
+        tasks.append(asyncio.ensure_future(check(item)))
+    loop.run_until_complete(asyncio.wait(tasks))
+checkmore = ' '
+def writefiles():
+    with open('proxies.txt', 'w+') as f:
+        for i in proxies:
+            f.write("%s\n" % i)
+        f.close()
+    with open('working.txt', 'w+') as f:
+        for i in working:
+            f.write("%s\n" % i)
+        f.close()
+    print('Saved')
+while checkmore != 'y':
+        main()
+        working = [*{*working}]
+        print(str(len(working)) + '  total working proxies')
+        proxies = [*{*proxies}]
+        writefiles()
